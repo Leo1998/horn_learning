3db6c7b5764b84fc39075fea01fa25d44fdb7ed6
Leo1998 <leonard.fricke98@gmail.com>
2023-06-13 16:26:48 +0200

    added first code for HORN1 algorithm

--- base.py
+++ base.py
@@ -7,6 +7,31 @@ BOTTOM = "0"
 
 Example = dict[str, bool]
 
+# Methods for Examples
+
+def true(e: Example) -> set[str]:
+    r = set()
+    for v in e.keys():
+        if e[v]:
+            r.add(v)
+    return r
+
+def false(e: Example) -> set[str]:
+    r = set()
+    for v in e.keys():
+        if not e[v]:
+            r.add(v)
+    return r
+
+def intersect(e1: Example, e2: Example) -> Example:
+    assert(len(e1) == len(e2))
+    r = dict()
+    for v in e1.keys():
+        r[v] = e1[v] and e2[v]
+    return r
+
+# Definitions for 'traditional' horn clauses
+
 class HornClause:
     def __init__(self, antecedent: list[str], consequent: Optional[str]):
         self.antecedent = antecedent
@@ -71,27 +96,6 @@ class HornFormula:
                 return False, e
         return True, None
 
-def true(e: Example) -> set[str]:
-    r = set()
-    for v in e.keys():
-        if e[v]:
-            r.add(v)
-    return r
-
-def false(e: Example) -> set[str]:
-    r = set()
-    for v in e.keys():
-        if not e[v]:
-            r.add(v)
-    return r
-
-def intersect(e1: Example, e2: Example) -> Example:
-    assert(len(e1) == len(e2))
-    r = dict()
-    for v in e1.keys():
-        r[v] = e1[v] and e2[v]
-    return r
-
 def covers(e: Example, c: HornClause) -> bool:
     return set(c.antecedent).issubset(true(e))
 
@@ -111,3 +115,97 @@ def gen_clauses(examples: list[Example]) -> list[HornClause]:
             clauses.append(HornClause(t, f))
         clauses.append(HornClause(t, None))
     return clauses
+
+# Definitions for 'meta' horn clauses
+
+class HornMetaClause:
+    def __init__(self, antecedent: list[str], consequent: list[str]):
+        self.antecedent = antecedent
+        self.consequent = consequent
+
+    def __str__(self):
+        if len(self.antecedent) == 0:
+            a = TOP
+        else:
+            a = "∧".join(sorted(list(self.antecedent)))
+        if len(self.consequent) == 0:
+            c = BOTTOM
+        else:
+            c = "∧".join(sorted(list(self.consequent)))
+        return f"({a} → {c})"
+    
+    def is_member(self, e: Example) -> bool:
+        for a in self.antecedent:
+            if not e[a]:
+                return True
+        if len(self.consequent) == 0:
+            return False
+        for c in self.consequent:
+            if not e[c]:
+                return False
+        return True
+
+class HornMetaFormula:
+    def __init__(self, clauses: list[HornMetaClause]):
+        self.clauses = clauses
+
+    def __str__(self):
+        f0 = map(str, self.clauses)
+        return "∧".join(f0)
+
+    def vars(self) -> list[str]:
+        v = set()
+        for c in self.clauses:
+            v.update(c.antecedent)
+            v.update(c.consequent)
+        return sorted(list(v))
+
+    def make_example(self, s: str) -> Example:
+        vars = self.vars()
+        assert(len(s) == len(vars))
+        result = dict()
+        for i, v in enumerate(vars):
+            result[v] = s[i] == "1"
+        return result
+
+    def is_member(self, e: Union[str, Example]) -> bool:
+        if type(e) is str:
+            e = self.make_example(e)
+        for c in self.clauses:
+            r = c.is_member(e)
+            if not r:
+                return False
+        return True
+
+    # TODO: replace with proper implementation! (currently just brute-forces all possible assignments)
+    def is_equivalent(self, f: Self) -> tuple[bool, Optional[Example]]:
+        vars = self.vars()
+        for i in range(math.floor(math.pow(2, len(vars)))):
+            e = self.make_example(bin(i)[2:].zfill(len(vars))) # hacky way to generate all possibilities
+
+            if self.is_member(e) != f.is_member(e):
+                return False, e
+        return True, None
+
+def covers_meta(e: Example, c: HornMetaClause) -> bool:
+    return set(c.antecedent).issubset(true(e))
+
+def violates_meta(e: Example, c: HornMetaClause) -> bool:
+    if not covers(e, c):
+        return False
+    if len(c.consequent) == 0:
+        return True
+    else:
+        for s in c.consequent:
+            if not e[s]:
+                return True
+        return False
+
+'''def gen_clauses_meta(examples: list[Example]) -> list[HornMetaClause]:
+    clauses = []
+    for e in examples:
+        t = list(true(e))
+        for f in false(e):
+            clauses.append(HornMetaClause(t, f))
+        clauses.append(HornMetaClause(t, None))
+    return clauses'''
--- horn1.py
+++ horn1.py
@@ -2,11 +2,11 @@
 
 import itertools
 
-from base import HornClause, HornFormula, Example, true, false, intersect, covers, violates, gen_clauses
+from base import HornMetaClause, HornMetaFormula, Example, true, false, intersect, covers_meta, violates_meta
 
-def horn(target: HornFormula) -> HornFormula:
+def horn1(target: HornMetaFormula) -> HornMetaFormula:
     S = []
-    H = HornFormula([])
+    H = HornMetaFormula([])
     while(True):
         equal, counter = target.is_equivalent(H)
         print(f"Current hypothesis: {H}")
@@ -16,11 +16,11 @@ def horn(target: HornFormula) -> HornFormula:
         else:
             if not H.is_member(counter): # is a positive counterexample
                 print(f"Positive counterexample: {counter}")
-                H.clauses = list(itertools.filterfalse(lambda c: violates(counter, c), H.clauses)) # remove from H every clause that x violates
+                # TODO
                 print("Eliminating incorrect clauses...")
             else: # is a negative counterexample
                 print(f"Negative counterexample: {counter}")
-                refined = False
+                '''refined = False
                 for i, s in enumerate(S):
                     intersected = intersect(s, counter)
                     if true(intersected).issubset(true(s)):
@@ -31,18 +31,18 @@ def horn(target: HornFormula) -> HornFormula:
                 if not refined:
                     print("Adding...")
                     S.append(counter)
-                H.clauses = gen_clauses(S) # regenerate H
+                H.clauses = gen_clauses(S) # regenerate H'''
             print("") # newline
 
 
 if __name__ == "__main__":
-    testClause1 = HornClause(["a", "c"], "d")
-    testClause2 = HornClause(["a", "b"], "c")
-    testFormula = HornFormula([testClause1, testClause2])
+    testClause1 = HornMetaClause(["a", "c"], ["d"])
+    testClause2 = HornMetaClause(["a", "b"], ["c"])
+    testFormula = HornMetaFormula([testClause1, testClause2])
 
     print(f"Target formula: {testFormula}\n")
 
-    horn(testFormula)
+    horn1(testFormula)
 
     #print(testFormula.is_member("1110"))
-    #print(testFormula.is_equivalent(HornFormula([testClause1])))
+    #print(testFormula.is_equivalent(HornMetaFormula([testClause1])))
